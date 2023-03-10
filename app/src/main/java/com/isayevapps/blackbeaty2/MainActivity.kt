24bbfc7528ab314520e90a5f012fd56fdfabba7e
elnurAndroidDev <isayeveln@gmail.com>
2023-03-10 19:08:13 +0500

    dialogs are created

--- app/src/main/java/com/isayevapps/blackbeaty2/MainActivity.kt
+++ app/src/main/java/com/isayevapps/blackbeaty2/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.isayevapps.blackbeaty2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lightTextView = findViewById<TextView>(R.id.lightTextView)
+
+        val rgbTextView = findViewById<TextView>(R.id.rgbTextView)
+
+        lightTextView.setOnClickListener {
+            showLightDialog()
+        }
+
+        rgbTextView.setOnClickListener {
+            showRGBDialog()
+        }
+
+
+    }
+
+    private fun showLightDialog() {
+        val builder = AlertDialog.Builder(this, R.style.AlertDialogStyle)
+        val inflater = layoutInflater
+        builder.setTitle("Яркость света")
+        val dialogLayout = inflater.inflate(R.layout.light_brightness_dialog, null)
+        builder.setView(dialogLayout)
+        builder.setPositiveButton("OK") { dialogInterface, _ -> dialogInterface.dismiss() }
+        builder.show()
+    }
+
+    private fun showRGBDialog() {
+        val builder = AlertDialog.Builder(this, R.style.AlertDialogStyle)
+        val inflater = layoutInflater
+        builder.setTitle("Управление RGB")
+        val dialogLayout = inflater.inflate(R.layout.rgb_control_dialog, null)
+        builder.setView(dialogLayout)
+        builder.setPositiveButton("OK") { dialogInterface, _ -> dialogInterface.dismiss() }
+        builder.show()
     }
 }
