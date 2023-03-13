3a17e24ddd49ab4393de487ed38ca819654b9caf
elnurAndroidDev <isayeveln@gmail.com>
2023-03-14 00:32:48 +0500

    worked on layouts

--- app/src/main/java/com/isayevapps/blackbeaty2/MainActivity.kt
+++ app/src/main/java/com/isayevapps/blackbeaty2/MainActivity.kt
@@ -1,63 +1,82 @@
 package com.isayevapps.blackbeaty2
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var lightAlertDialog: AlertDialog
+    private lateinit var rgbAlertDialog: AlertDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewModel = (application as App).viewModel
-        viewModel.init(this, savedInstanceState == null)
+        prepareLightDialog()
+        prepareRGBDialog()
 
-        viewModel.states.observe(this) {
-            when (it) {
-                is States.WaitForConnection -> {
-                }
-                is States.Connection -> {
-                    viewModel.searchDevice()
-                }
-                is States.Connected -> {
+        /* val viewModel = (application as App).viewModel
+         viewModel.init(this, savedInstanceState == null)
 
-                }
-            }
+         viewModel.states.observe(this) {
+             when (it) {
+                 is States.WaitForConnection -> {
+                 }
+                 is States.Connection -> {
+                     viewModel.searchDevice()
+                 }
+                 is States.Connected -> {
+
+                 }
+             }
+         }*/
+
+        val connectionStatusLayout = findViewById<FrameLayout>(R.id.connectionStatusLayout)
+        connectionStatusLayout.visibility = View.GONE
+
+
+        val lightTextView = findViewById<TextView>(R.id.lightTextView)
+        val rgbTextView = findViewById<TextView>(R.id.rgbTextView)
+        val seatButton = findViewById<AppCompatButton>(R.id.seatButton)
+
+        seatButton.setOnClickListener {
+            startActivity(Intent(this, SeatActivity::class.java))
         }
 
-        /* val lightTextView = findViewById<TextView>(R.id.lightTextView)
+        lightTextView.setOnClickListener {
+            lightAlertDialog.show()
+        }
 
-         val rgbTextView = findViewById<TextView>(R.id.rgbTextView)
-
-         lightTextView.setOnClickListener {
-             showLightDialog()
-         }
-
-         rgbTextView.setOnClickListener {
-             showRGBDialog()
-         }*/
+        rgbTextView.setOnClickListener {
+            rgbAlertDialog.show()
+        }
 
     }
 
-    private fun showLightDialog() {
+    private fun prepareLightDialog() {
         val builder = AlertDialog.Builder(this, R.style.AlertDialogStyle)
         val inflater = layoutInflater
         builder.setTitle("Яркость света")
         val dialogLayout = inflater.inflate(R.layout.light_brightness_dialog, null)
         builder.setView(dialogLayout)
         builder.setPositiveButton("OK") { dialogInterface, _ -> dialogInterface.dismiss() }
-        builder.show()
+        lightAlertDialog = builder.create()
     }
 
-    private fun showRGBDialog() {
+    private fun prepareRGBDialog() {
         val builder = AlertDialog.Builder(this, R.style.AlertDialogStyle)
         val inflater = layoutInflater
         builder.setTitle("Управление RGB")
         val dialogLayout = inflater.inflate(R.layout.rgb_control_dialog, null)
         builder.setView(dialogLayout)
         builder.setPositiveButton("OK") { dialogInterface, _ -> dialogInterface.dismiss() }
-        builder.show()
+        rgbAlertDialog = builder.create()
     }
 }
--- app/src/main/java/com/isayevapps/blackbeaty2/SeatActivity.kt
+++ app/src/main/java/com/isayevapps/blackbeaty2/SeatActivity.kt
@@ -1,11 +1,86 @@
 package com.isayevapps.blackbeaty2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import com.isayevapps.blackbeaty2.databinding.ActivitySeatBinding
 
 class SeatActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySeatBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_seat)
+        binding = ActivitySeatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.seatPos1Button.setOnClickListener {
+            selectPos(1)
+        }
+        binding.seatPos2Button.setOnClickListener {
+            selectPos(2)
+        }
+        binding.seatPos3Button.setOnClickListener {
+            selectPos(3)
+        }
+        binding.seatPos4Button.setOnClickListener {
+            selectPos(4)
+        }
+
+        binding.headButton.setOnClickListener {
+            selectPart(1)
+            binding.seatPartUpButton.visibility = View.VISIBLE
+            binding.seatPartDownButton.visibility = View.VISIBLE
+            binding.seatPartLeftButton.visibility = View.GONE
+            binding.seatPartRightButton.visibility = View.GONE
+        }
+        binding.backButton.setOnClickListener {
+            selectPart(2)
+            binding.seatPartUpButton.visibility = View.GONE
+            binding.seatPartDownButton.visibility = View.GONE
+            binding.seatPartLeftButton.visibility = View.VISIBLE
+            binding.seatPartRightButton.visibility = View.VISIBLE
+        }
+        binding.legButton.setOnClickListener {
+            selectPart(3)
+            binding.seatPartUpButton.visibility = View.VISIBLE
+            binding.seatPartDownButton.visibility = View.VISIBLE
+            binding.seatPartLeftButton.visibility = View.GONE
+            binding.seatPartRightButton.visibility = View.GONE
+        }
+        binding.moveButton.setOnClickListener {
+            selectPart(4)
+            binding.seatPartUpButton.visibility = View.GONE
+            binding.seatPartDownButton.visibility = View.GONE
+            binding.seatPartLeftButton.visibility = View.VISIBLE
+            binding.seatPartRightButton.visibility = View.VISIBLE
+        }
+    }
+
+    private fun selectPos(i: Int) {
+        val buttonBGDrawable =
+            ResourcesCompat.getDrawable(resources, R.drawable.button_bg, null)
+        val buttonSelectedBGDrawable =
+            ResourcesCompat.getDrawable(resources, R.drawable.button_selected_bg, null)
+        binding.seatPos1Button.background =
+            if (i == 1) buttonSelectedBGDrawable else buttonBGDrawable
+        binding.seatPos2Button.background =
+            if (i == 2) buttonSelectedBGDrawable else buttonBGDrawable
+        binding.seatPos3Button.background =
+            if (i == 3) buttonSelectedBGDrawable else buttonBGDrawable
+        binding.seatPos4Button.background =
+            if (i == 4) buttonSelectedBGDrawable else buttonBGDrawable
+    }
+
+    private fun selectPart(i: Int) {
+        val buttonBGDrawable =
+            ResourcesCompat.getDrawable(resources, R.drawable.button_bg, null)
+        val buttonSelectedBGDrawable =
+            ResourcesCompat.getDrawable(resources, R.drawable.button_selected_bg, null)
+        binding.headButton.background = if (i == 1) buttonSelectedBGDrawable else buttonBGDrawable
+        binding.backButton.background = if (i == 2) buttonSelectedBGDrawable else buttonBGDrawable
+        binding.legButton.background = if (i == 3) buttonSelectedBGDrawable else buttonBGDrawable
+        binding.moveButton.background = if (i == 4) buttonSelectedBGDrawable else buttonBGDrawable
     }
 }
